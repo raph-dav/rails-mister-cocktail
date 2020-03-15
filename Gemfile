ab0885a7929f89b6cb836b4daaf9df75f0457aa0
Raphael Daverio <raphael.daverio@gmail.com>
2020-03-15 20:04:51 +0100

    Initial commit

--- Gemfile
+++ Gemfile
@@ -57,3 +57,9 @@ end
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 gem 'rspec-rails', group: [ :test ]
 gem 'rails-controller-testing', group: [ :test ]
+
+gem 'pry-byebug'
+
+gem 'autoprefixer-rails'
+gem 'simple_form'
+
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'json'
+require 'open-uri'
+
+puts 'destroying all ingredients'
+Ingredient.destroy_all
+
+puts 'creating new ingredients'
+
+url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
+cocktails_opened = open(url).read
+cocktails_parsed = JSON.parse(cocktails_opened)
+
+cocktails_parsed["drinks"].each do |ingredient|
+  new_ingredient = Ingredient.new(name: ingredient["strIngredient1"])
+  new_ingredient.save
+end
+
+puts "I'm done! you have 10 new ingredients in your DB"
